39f4bf5d51728f662dc0e859fae4f55cad0d7e61
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2021-10-31 16:43:54 +0500

    Handle the list key warning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function Hello(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.message}</h1>
+      <h1>Welcome to the {props.libray}!</h1>
+      <h2>You will the enjoy the course!</h2>
+    </div>
+  );
+}
+
+function ShowList({list}){
+  return (
+    <div>
+      <ul>
+        {
+          list.map((item) => {
+            return <li key={item.id}>{item.name}</li>
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+const list = ['To go to UET', 'Apply for Degree', 'Collect DMC', 'Meet Haseeb', 'Eat Something'];
+const ListObj = list.map((item, i) => ({id:i, name: item}));
+console.log(ListObj);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello message="Hurray!" libray="Next"/>
+      <ShowList list={ListObj}/>
+      <h4>Footer</h4>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
+/*
+React.createElement(
+  "h1", tag
+  null, property
+  "Header" content
+)
+*/
 ReactDOM.render(
   <React.StrictMode>
     <App />
